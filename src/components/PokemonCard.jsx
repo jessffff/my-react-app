384806020e55146e846e50eca1cf9c9ff77d67e0
jessffff <jessicabjb@gmail.com>
2023-12-19 10:30:06 +0100

    bulbasaur

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const PokemonCard = () => {
+function PokemonCard  ()  {
     return (
         <figure>
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
