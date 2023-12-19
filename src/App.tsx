642e57f1cacc7bd66a95d723a983764637e41280
jessffff <122911304+jessffff@users.noreply.github.com>
2023-12-19 11:45:44 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,30 @@ import NavBar from './components/NavBar'
 import './App.css'
 
 const pokemonList = [
-  {
-    name: "Bulbasaur",
+{
+    name: "bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-    name: "Mew",
-    imgSrc: null, // Mew n'a pas d'image définie
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
   },
 ];
-
 
 function App() {
   const [pokemonIndex, setPokemonIndex] = useState(0);
