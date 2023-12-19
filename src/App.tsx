8b1a75f6065a7c588a029c945e98a7d0de9a5e90
jessffff <jessicabjb@gmail.com>
2023-12-19 11:49:38 +0100

    event

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import PokemonCard from './components/PokemonCard' 
 import NavBar from './components/NavBar'
 import './App.css'
 
 const pokemonList = [
   {
-    name: "Bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "Mew",
-    imgSrc: null, // Mew n'a pas d'image définie
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 
 function App() {
@@ -22,7 +36,9 @@ function App() {
   const handlePokemonClick = (index) => {
       setPokemonIndex(index);
   };
-
+  useEffect(() => {
+    alert("hello pokemon trainer :)");
+  }, []);
   return (
       <div>
           <NavBar pokemonList={pokemonList} onPokemonClick={handlePokemonClick} />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
 function NavBar({ pokemonList, onPokemonClick }) {
+    const handlePokemonSelection = (index) => {
+        const selectedPokemon = pokemonList[index];
+        if (selectedPokemon.name.toLowerCase() === 'pikachu') {
+            alert('pika pikachu !!!');
+        }
+        onPokemonClick(index);
+    };
+
     return (
         <nav>
             {pokemonList.map((pokemon, index) => (
-                <button key={pokemon.name} onClick={() => onPokemonClick(index)}>
+                <button key={pokemon.name} onClick={() => handlePokemonSelection(index)}>
                     {pokemon.name}
                 </button>
             ))}
